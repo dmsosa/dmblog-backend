c2dae7e1a3035b567335b54a08d6b09982ef7932
Durian Sosa <dmsosa21@outlook.com>
2024-05-07 10:47:08 +0100

    Backend in separated repository

---
+++ blog-service/src/main/java/com/duvi/blogservice/model/dto/CommentDTO.java
@@ -0,0 +1,8 @@
+package com.duvi.blogservice.model.dto;
+
+import jakarta.annotation.Nullable;
+
+import java.time.LocalDateTime;
+
+public record CommentDTO(Long id, Long articleId, String image, String username, String body, @Nullable LocalDateTime postedAt, @Nullable LocalDateTime updatedAt) {
+}
